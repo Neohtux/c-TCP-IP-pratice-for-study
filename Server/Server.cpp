7ef26667c8386f8c200cb194bd6fe5fba98e77c9
Neohtux <neohtux@daum.net>
2020-12-24 16:03:33 +0900

    2020-12-24 Server 커밋 내용  - 기본 특이사항 없음.

---
+++ Server/Server.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include "Listener.h"
+#include "SessionManager.h"
+using namespace boost::asio;
+SessionManager* _sessionManager;
+int main()
+{
+	_sessionManager = SessionManager::GetInstance();
+	try
+	{
+		boost::asio::io_context iocontext;
+		ip::tcp::endpoint ep(ip::tcp::v4(), PORT_NUMBER); //ipv4, 포트번호
+		auto _listener = std::make_unique<Listener>(iocontext, ep);
+
+		_listener->Start_accept();
+		iocontext.run();
+
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << '\n';
+	}
+	
+
+	/*while (true) 
+	{
+		int w = _sessionManager->_sessionMap->size();
+		int q = 1;
+		int m = 3;
+		std::cout << "asdasdsadasdasdadsad \n" << '\n';
+	}*/
+	return 0;
+
+}
